6981f95e0a8b903cbc042bd08762b1d14d894072
Chris Castle <chris@render.com>
2021-09-08 16:00:10 -0700

    🌱🐍Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
